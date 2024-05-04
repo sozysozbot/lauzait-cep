3df8149ff24f755ce143dc2ec35176f993a839ee
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-05-04 23:07:44 +0900

    「天和であるとき、筆兵無傾である確率」

--- index.ts
+++ index.ts
@@ -22,20 +22,47 @@ console.log({
 
 function calculateTianhuProbabilityForSingleSample(): number {
   const { PLAYER_1_HAND } = createNewInitialConfig();
-  if (isHule(PLAYER_1_HAND)) { 
+  if (isHule(PLAYER_1_HAND)) {
     // console.log(PLAYER_1_HAND);
-    return 1; } return 0;
+    return 1;
+  } return 0;
 }
 
 function calculateTianhuProbability(total: number): number {
-  let stuck = 0;
+  let hule = 0;
   for (let i = 1; i <= total; i++) {
     if (!(i % 100000)) {
-      console.log(`${i}\t/${total} done. Current estimate:\t${stuck / i}`)
+      console.log(`${i}\t/${total} done. Current estimate:\t${hule / i}`)
     }
-    stuck += calculateTianhuProbabilityForSingleSample();
+    hule += calculateTianhuProbabilityForSingleSample();
   }
-  return stuck / total;
+  return hule / total;
 }
 
-console.log(calculateTianhuProbability(3e8));
+// console.log(calculateTianhuProbability(3e8));
+
+function is_balanced(cards: Card[]) {
+  const count_筆 = cards.map(c => { const rank: number = c.suit === "筆" ? c.rank : 0; return rank; }).reduce((a, b) => a + b, 0);
+  const count_兵 = cards.map(c => { const rank: number = c.suit === "兵" ? c.rank : 0; return rank; }).reduce((a, b) => a + b, 0);
+  return count_兵 === count_筆;
+}
+
+function calculateBalancedGivenTianhuProbability(total: number): number {
+  let hule = 0;
+  let balanced = 0;
+  for (let i = 1; i <= total; i++) {
+    if (!(i % 100000)) {
+      console.log(`${i}\t/${total} done. Current estimate:\t${balanced / hule}`)
+    }
+
+    const { PLAYER_1_HAND } = createNewInitialConfig();
+    if (isHule(PLAYER_1_HAND)) {
+      if (is_balanced(PLAYER_1_HAND)) { balanced++; }
+      hule += 1;
+    }
+  }
+  return balanced / hule;
+}
+
+console.log(calculateBalancedGivenTianhuProbability(1e6));
+
